63a8283fd44e48617c7b6a4252f076b28ecf5129
PendiMaulana001 <pendimaulana001@gmail.com>
2024-12-31 22:27:45 +0700

    Add files via upload

---
+++ model_parameters_pesetron_fish.js
@@ -0,0 +1,6 @@
+
+const svmWeights = [-40.73165456948075, 7.409095223847764, -11.542767692036893];
+const svmIntercept = -31.0;
+const scalerMean = [17.437613357843137, 3.741390931372549, 0.2513174019607843];
+const scalerScale = [7.116184917369974, 1.0477823051270563, 0.12294802919708096];
+const classes = ['Anabas testudineus', 'Coilia dussumieri', 'Otolithoides biauritus', 'Otolithoides pama', 'Pethia conchonius', 'Polynemus paradiseus', 'Puntius lateristriga', 'Setipinna taty', 'Sillaginopsis panijus'];
